971d8f3737f0d08f48773e03a5956cd77804d370
unknown <jdolinger@.lab49.com>
2011-05-11 18:51:59 -0400

    ack new file....

---
+++ Jd.Wpf.Validation/Util/RunAfterDispatchCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace Jd.Wpf.Validation.Util
+{
+    using System;
+    using System.Windows.Threading;
+
+    public class RunAfterDispatchCommand
+    {
+        private readonly Action code;
+        private bool activeWaiting;
+
+        public RunAfterDispatchCommand(Action code)
+        {
+            this.code = code;
+        }
+
+        public bool ActiveWaiting
+        {
+            get { return this.activeWaiting; }
+        }
+
+        public void Execute()
+        {
+            this.activeWaiting = true;
+            Dispatcher.CurrentDispatcher.BeginInvoke(new Action(() =>
+            {
+                this.code();
+                this.activeWaiting = false;
+            }));
+        }
+    }
+}
