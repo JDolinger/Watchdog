f766fbffe02dec9f4829633122903a00b65d586a
unknown <jdolinger@.lab49.com>
2011-05-15 12:31:36 -0400

    Renamespaced App.

--- Watchdog.Examples/App.xaml.cs
+++ Watchdog.Examples/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Windows;
 
-namespace Jd.Wpf.Validation.Examples
+namespace Watchdog.Examples
 {
     /// <summary>
     /// Interaction logic for App.xaml
