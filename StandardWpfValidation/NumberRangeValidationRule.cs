ad6ab5b41254d422fe225d78d2d1a229b8977654
unknown <jdolinger@.lab49.com>
2011-05-15 21:45:44 -0400

    Project to show basic samples.

---
+++ StandardWpfValidation/NumberRangeValidationRule.cs
@@ -0,0 +1,24 @@
+﻿namespace StandardWpfValidation
+{
+    using System.Globalization;
+    using System.Windows.Controls;
+
+    public class NumberRangeValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int intVal;
+            if (!int.TryParse(value.ToString(), out intVal))
+            {
+                return new ValidationResult(false, "not a valid integer");
+            }
+
+            if (intVal < 25 || intVal > 75)
+            {
+                return new ValidationResult(false, "Out of allowed interval");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ StandardWpfValidation/ViewModel.cs
@@ -0,0 +1,99 @@
+﻿namespace Watchdog.Examples.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+
+    public class ViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private string text;
+        private string userId;
+        private string password1;
+        private string password2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get { return this.text; }
+            set
+            {
+                this.text = value;
+                this.RaisePropertyChanged("Text");
+            }
+        }
+
+        public string UserId
+        {
+            get { return this.userId; }
+            set
+            {
+                this.userId = value;
+                this.RaisePropertyChanged("UserId");
+            }
+        }
+
+        public string Password1
+        {
+            get { return this.password1; }
+            set
+            {
+                this.password1 = value;
+                this.RaisePropertyChanged("Password1");
+                this.RaisePropertyChanged("Password2");
+            }
+        }
+
+        public string Password2
+        {
+            get { return this.password2; }
+            set
+            {
+                this.password2 = value;
+                this.RaisePropertyChanged("Password1");
+                this.RaisePropertyChanged("Password2");
+            }
+        }
+
+        protected virtual void RaisePropertyChanged(string name)
+        {
+            var h = this.PropertyChanged;
+            if (h != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        #region IDataErrorInfo Members
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == "Password1")
+                {
+                    if (!string.Equals(this.password1, this.password2))
+                    {
+                        return "Passwords must match";
+                    }
+                }
+
+                if (columnName == "Password2")
+                {
+                    if (!string.Equals(this.password1, this.password2))
+                    {
+                        return "Passwords must match";
+                    }
+                }
+
+                return string.Empty;
+            }
+        }
+
+        #endregion
+    }
+}
