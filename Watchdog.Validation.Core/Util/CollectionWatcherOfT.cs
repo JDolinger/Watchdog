0a13892fbb9bdc805add460f012df5bcbae9b360
unknown <jdolinger@.lab49.com>
2011-05-17 09:52:57 -0400

    changed main window of application to demodriver.xaml

--- Watchdog.Validation.Core/Util/CollectionWatcherOfT.cs
+++ Watchdog.Validation.Core/Util/CollectionWatcherOfT.cs
@@ -30,40 +30,41 @@ namespace Watchdog.Validation.Core.Util
     using System.Linq;
 
     /// <summary>
-    ///     A utility which watches a <see cref = "ObservableCollection{T}" />.  When the
-    ///     collection raises <see cref = "System.Collections.Specialized.INotifyCollectionChanged" /> event, it
-    ///     calls back the provided Actions corresponding to each type of event.  This class simple helps you
-    ///     move the typical boilerplate out of the classes which must be respond to these events.
+    /// A utility which watches a <see cref="ObservableCollection{T}"/>.  When the
+    /// collection raises <see cref="System.Collections.Specialized.INotifyCollectionChanged"/> event, it
+    /// calls back the provided Actions corresponding to each type of event.  This class simple helps you
+    /// move the typical boilerplate out of the classes which must be respond to these events.
     /// </summary>
-    /// <typeparam name = "T">The type contained in the collection being watched.</typeparam>
+    /// <typeparam name="T">The type contained in the collection being watched.</typeparam>
+    /// <typeparam name="TFilter">The type of the filter.</typeparam>
     public class CollectionWatcher<T, TFilter>
     {
         /// <summary>
-        ///     Callback invoked for each <see cref = "T" /> that gets added to the collection.
+        /// Callback invoked for each <see cref="T"/> that gets added to the collection.
         /// </summary>
         private readonly Action<TFilter> addHandler;
 
         /// <summary>
-        ///     Callback invoked for each <see cref = "T" /> that gets removed from the collection.
+        /// Callback invoked for each <see cref="T"/> that gets removed from the collection.
         /// </summary>
         private readonly Action<TFilter> removeHandler;
 
         /// <summary>
-        ///     Callback invoked when the collection has dramatically changed.
+        /// Callback invoked when the collection has dramatically changed.
         /// </summary>
         private readonly Action resetHandler;
 
         /// <summary>
-        ///     The collection under watch.
+        /// The collection under watch.
         /// </summary>
         private ObservableCollection<T> collection;
 
         /// <summary>
-        ///     Initializes a new instance of the <see cref = "CollectionWatcher&lt;T&gt;" /> class.
+        /// Initializes a new instance of the <see cref="CollectionWatcher&lt;T&gt;"/> class.
         /// </summary>
-        /// <param name = "addHandler">The add handler.</param>
-        /// <param name = "removeHandler">The remove handler.</param>
-        /// <param name = "resetHandler">The reset handler.</param>
+        /// <param name="addHandler">The add handler.</param>
+        /// <param name="removeHandler">The remove handler.</param>
+        /// <param name="resetHandler">The reset handler.</param>
         public CollectionWatcher(Action<TFilter> addHandler, Action<TFilter> removeHandler, Action resetHandler)
         {
             if (addHandler == null)
@@ -87,8 +88,8 @@ namespace Watchdog.Validation.Core.Util
         }
 
         /// <summary>
-        ///     Sets the <see cref = "System.Collections.ObjectModel.ObservableCollection{T}"> that will be observed, 
-        ///                  and hooks up to the <see cref = "CollectionChanged" /> event.
+        /// Sets the <see cref = "System.Collections.ObjectModel.ObservableCollection{T}"> that will be observed, 
+        /// and hooks up to the <see cref = "CollectionChanged" /> event.
         /// </summary>
         /// <param name = "newCollection">The new collection.</param>
         public void Watch(ObservableCollection<T> newCollection)
@@ -119,13 +120,11 @@ namespace Watchdog.Validation.Core.Util
         }
 
         /// <summary>
-        ///     Listens for the different CollectionChanged events, and appropriately calls back
-        ///     each registered <see cref = "Action{T}" /> for each added or removed <see cref = "T" />
+        /// Listens for the different CollectionChanged events, and appropriately calls back
+        /// each registered <see cref="Action{T}"/> for each added or removed <see cref="T"/>
         /// </summary>
-        /// <param name = "sender">The sender.</param>
-        /// <param name = "e">
-        ///     The <see cref = "System.Collections.Specialized.NotifyCollectionChangedEventArgs" /> instance containing the event data.
-        /// </param>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.Collections.Specialized.NotifyCollectionChangedEventArgs"/> instance containing the event data.</param>
         private void HandleCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             switch (e.Action)
@@ -143,10 +142,10 @@ namespace Watchdog.Validation.Core.Util
         }
 
         /// <summary>
-        ///     Iterates the list of any added <see cref = "T" /> 
-        ///     and invokes the <see cref = "addHandler" /> callback.
+        /// Iterates the list of any added <see cref="T"/>
+        /// and invokes the <see cref="addHandler"/> callback.
         /// </summary>
-        /// <param name = "newItems">The list of new item.</param>
+        /// <param name="newItems">The list of new item.</param>
         private void ReportAdds(IList newItems)
         {
             foreach (var i in newItems)
@@ -161,10 +160,10 @@ namespace Watchdog.Validation.Core.Util
         }
 
         /// <summary>
-        ///     Iterates the list of any removed <see cref = "T" /> 
-        ///     and invokes the <see cref = "removeHandler" /> callback.
+        /// Iterates the list of any removed <see cref="T"/>
+        /// and invokes the <see cref="removeHandler"/> callback.
         /// </summary>
-        /// <param name = "removedItems">The list of new item.</param>
+        /// <param name="removedItems">The list of new item.</param>
         private void ReportRemoves(IList removedItems)
         {
             foreach (var i in removedItems.OfType<TFilter>())
